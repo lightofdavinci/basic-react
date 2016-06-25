8eb09459db3f18e53a9a91c0b29d59510f535f02
lightofdavinci <myrovoy@mail.ru>
2016-06-25 19:02:51 -0400

    changing babel

--- js/main.js
+++ js/main.js
@@ -1,24 +1,31 @@
+"use strict";
+
 var Main = React.createClass({
-getInitialState(){
-     return {
-       converter: new showdown.Converter(),
-       value: 'Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\n *[made by Dan Volosnikov](https://freecodecamp.com/lightofdavinci)*',}
- },
- update(event){
-this.setState({value: event.target.value});
- }, 
- render(){
-    return (
-      <div className="row">
-      <div className="col-md-6"> 
-      <textarea type="text" className="form-control" id="leArea" rows="15" defaultValue={this.state.value} onChange={this.update}></textarea>
-      </div>
-      <div className="col-md-6" id="box">
-      <div dangerouslySetInnerHTML={{__html: this.state.converter.makeHtml(this.state.value)}} />
-      </div>
-      </div>
-    )
+  displayName: "Main",
+  getInitialState: function getInitialState() {
+    return {
+      converter: new showdown.Converter(),
+      value: 'Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\n *[made by Dan Volosnikov](https://freecodecamp.com/lightofdavinci)*' };
+  },
+  update: function update(event) {
+    this.setState({ value: event.target.value });
+  },
+  render: function render() {
+    return React.createElement(
+      "div",
+      { className: "row" },
+      React.createElement(
+        "div",
+        { className: "col-md-6" },
+        React.createElement("textarea", { type: "text", className: "form-control", id: "leArea", rows: "15", defaultValue: this.state.value, onChange: this.update })
+      ),
+      React.createElement(
+        "div",
+        { className: "col-md-6", id: "box" },
+        React.createElement("div", { dangerouslySetInnerHTML: { __html: this.state.converter.makeHtml(this.state.value) } })
+      )
+    );
   }
 });
 
-ReactDOM.render(<Main />, document.getElementById('app'));
+ReactDOM.render(React.createElement(Main, null), document.getElementById('app'));
